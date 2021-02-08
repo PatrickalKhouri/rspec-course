1d6e7f38bfebca3ac493fa155dc9b68c72118f6f
Patrick Khouri <patrick.darya@gmail.com>
2021-02-08 20:16:33 -0300

    ...

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,33 @@
 class Card
-  attr_reader :type
+  attr_accessor :rank, :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
 
-RSpec.describe 'Card' do 
-  it 'has a type' do
-    card = Card.new("Ace of Spades")
-    expect(card.type).to eq('Ace of Spades')
+RSpec.describe Card do 
+  let(:card) { Card.new('Ace', 'Spades') }
+
+  before do 
+    @card = Card.new('Ace', 'Spades')
   end
+
+  it 'has a rank and it can change' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom error message' do
+    comparisson = 'Spade'
+    expect(card.suit).to eq(comparisson), "I expected #{comparisson} to equal Spades"
+  end
+
 end
 
